99190eae6e35dc4f3b6b50709a8664f3768c977c
codersalman <coder88salman@gmail.com>
2021-07-17 20:22:54 +0530

    Initial commit

---
+++ app/src/main/java/com/caresofts/netset/Item/MyReceiver.java
@@ -0,0 +1,18 @@
+package com.caresofts.netset.Item;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        String status = NetworkUtil.getConnectivityStatusString(context);
+        if(status.isEmpty()) {
+            status="No Internet Connection";
+        }
+        Toast.makeText(context, status, Toast.LENGTH_LONG).show();
+    }
+}
